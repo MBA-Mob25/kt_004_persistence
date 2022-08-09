822a257f731feefc066030e67cc8e3e8dc12e282
Marcelo Mussi <mussi@prodev.com.br>
2022-08-09 20:04:00 -0300

    adicionando comentarios da atividade

--- app/src/main/java/com/example/kt_004_shared_preferences/MainActivity.kt
+++ app/src/main/java/com/example/kt_004_shared_preferences/MainActivity.kt
@@ -10,26 +10,41 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Função responsável pela configuração das ações ao carregar a tela
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Vamos configurar as funções que serão executadas na tela principal
+        // Neste caso, ao clicar vamos salvar os dados
         btnSave.setOnClickListener(View.OnClickListener { onSave() });
+        // Neste caso, ao clicar vamos carregar outra activity e mostrar o resultado
         btnShow.setOnClickListener(View.OnClickListener { onShow() })
     }
 
+    // Função responsável por salvar os dados
     private fun onSave() {
+        // Carregando a biblioteca de preferences nativa do android
         val prefs = this.getSharedPreferences("storageTreatment", Context.MODE_PRIVATE)
+        // Pegando o objeto responsável pela edição dos dados dentro das preferencias do sistema
         val editor = prefs.edit()
 
+        // Adicionando os dados preenchidos pelo usuário
         editor.putString("name", inputName.text.toString())
         editor.putString("treatment", listTreatment.selectedItem.toString())
+
+        // Aplicando os dados de preferencia / salvando na memória
         editor.apply()
 
+        // Exibindo uma mensagem para o usuário dizendo que tudo foi salvo com sucesso
         Toast.makeText(this, "Dados salvos com sucesso", Toast.LENGTH_SHORT).show();
     }
 
+    // Função responsável pela navegação até a exibição da tela de saudação
     private fun onShow() {
+        // Carregando a intenção da nova tela para o usuário navegar
         val intent = Intent(this, TreatmentActivity::class.java)
+        // Executando a navegação para nova tela
         startActivity(intent)
     }
 
--- app/src/main/java/com/example/kt_004_shared_preferences/TreatmentActivity.kt
+++ app/src/main/java/com/example/kt_004_shared_preferences/TreatmentActivity.kt
@@ -6,20 +6,29 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_treatment.*
 
 class TreatmentActivity : AppCompatActivity() {
+
+    // Função responsável pela configuração das ações ao carregar a tela
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_treatment)
+        // Chamando a função que será responsável por carregar os dados salvos
         loadPrefs()
     }
 
+    // Função que vai carregar os dados salvos pelo usuários
     private fun loadPrefs() {
+        // Carregando a biblioteca de preferences nativa do android
         val prefs = this.getSharedPreferences("storageTreatment", Context.MODE_PRIVATE)
+        // Carergando as preferencias salvas pelo usuário na tela anterior e armazenando em uma variável
         val name = prefs.getString("name", "")
         val treatment = prefs.getString("treatment", "")
 
+        // Vamos verificar se exite um tratamento configurado
         if (treatment == "Sem tratamento") {
+            // Caso nenhum tratamento seja a opção selecioada, vamos exibir apenas o nome
             showTreatment.text = name;
         } else {
+            // Se existir um tratamento configurado, vamos concatenar os dados e exibir a mensagem
             val fullText = "$treatment $name";
             showTreatment.text = fullText
         }
